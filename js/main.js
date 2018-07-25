aab3263f49ee98494db71d96bfb0518599e2fd76
Slawomir Wojda <slwojda@poczta.onet.pl>
2018-07-25 22:19:20 +0200

    sprawdzenie liczb

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function porownajLiczbe(parametr) {
+    if (parametr >= 100 && parametr <= 200) {
+        console.log("Liczba znajduje się w przedziale");
+    } else console.log("Liczba nie znajduje się w przedziale");
+
+}
+
+
+porownajLiczbe(199);
